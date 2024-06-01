e1caaf5effa6f7cc285c054f366b4d684433cfa7
EJ Fox <ejfox@ejfox.com>
2024-06-01 14:38:28 -0400

    save game (searching works)

--- index.mjs
+++ index.mjs
@@ -6,6 +6,7 @@ import * as d3 from "d3";
 import chalk from "chalk";
 import { format } from "date-fns";
 import { execSync } from "child_process";
+import Fuse from "fuse.js";
 
 (async () => {
   const instructions = `Welcome to the scrapbook CLI! Up/down keys navigate, right arrow copies the URL to the clipboard. Press spacebar to view the summary. Press q to quit.`;
@@ -23,6 +24,7 @@ import { execSync } from "child_process";
   };
 
   const bookmarks = await loadBookmarks();
+  let currentBookmarks = bookmarks;
 
   bookmarks.forEach((bookmark) => {
     bookmark[
@@ -31,6 +33,19 @@ import { execSync } from "child_process";
   });
 
   bookmarks.sort((a, b) => new Date(b.time) - new Date(a.time));
+
+  const options = {
+    threshold: 0.3,
+    distance: 100,
+    ignoreLocation: true,
+    includeScore: true,
+    minMatchCharLength: 3,
+  };
+
+  const fuse = new Fuse(bookmarks, {
+    keys: ["description", "extended", "tags"],
+    ...options,
+  });
 
   const scrapTypes = Array.from(new Set(bookmarks.map((b) => b.type)));
   const colorScale = d3.scaleOrdinal(d3.schemeCategory10).domain(scrapTypes);
@@ -52,7 +67,6 @@ import { execSync } from "child_process";
       return row;
     }),
   };
-
   const program = new Command();
 
   program
@@ -108,9 +122,27 @@ import { execSync } from "child_process";
         },
       });
 
+      const summaryBox = blessed.box({
+        parent: screen,
+        top: "center",
+        left: "center",
+        height: "80%",
+        width: "80%",
+        border: null,
+        padding: 1,
+        style: {
+          border: {
+            fg: "green",
+          },
+        },
+      });
+
+      screen.append(summaryBox);
+      summaryBox.hide();
+
       const coloredTableData = {
         headers: tableData.headers,
-        data: tableData.data.map((row) => {
+        data: tableData.data.map((row, index) => {
           const color = row.color;
           return row.map((cell) => chalk.hex(color)(cell));
         }),
@@ -123,14 +155,19 @@ import { execSync } from "child_process";
       screen.key(["escape", "q", "C-c"], () => process.exit(0));
 
       let currentSummaryInterval;
-      const animDuration = 50;
-      const lettersAtAATime = 3;
+      const animDuration = 10;
+      const lettersAtAATime = 8;
       const viewSummary = (index) => {
+        if (summaryBox.visible) {
+          summaryBox.hide();
+          screen.render();
+        }
+
         if (currentSummaryInterval) {
           clearInterval(currentSummaryInterval);
         }
 
-        const bookmark = bookmarks[index];
+        const bookmark = currentBookmarks[index]; // Use currentBookmarks instead of bookmarks
         const summary = headers
           .map((header) => `${header}: ${bookmark[header]}`)
           .join("\n\n");
@@ -161,21 +198,26 @@ import { execSync } from "child_process";
         }
       };
 
-      table.rows.on("select", (item, index) => {
+      const updateSummary = (index) => {
         stopCurrentAnimation();
         viewSummary(index);
+      };
+
+      screen.key(["j", "down"], () => {
+        updateSummary(table.rows.selected + 1);
+        screen.render();
       });
 
-      // screen.key(["space"], () => {
-      //   const selected = table.rows.selected;
-      //   stopCurrentAnimation();
-      //   viewSummary(selected);
-      // });
+      screen.key(["k", "up"], () => {
+        updateSummary(table.rows.selected - 1);
+        screen.render();
+      });
 
-      // open the URL in browser on space
       screen.key(["space"], () => {
         const selected = table.rows.selected;
-        const bookmark = bookmarks[selected];
+        updateSummary(selected);
+        const currentBookmarks = bookmarks;
+        const bookmark = currentBookmarks[selected];
         const href = bookmark["href"];
         try {
           execSync(`open ${href}`);
@@ -191,21 +233,9 @@ import { execSync } from "child_process";
         screen.render();
       });
 
-      screen.key(["j", "down"], () => {
-        stopCurrentAnimation();
-        viewSummary(table.rows.selected);
-        screen.render();
-      });
-
-      screen.key(["k", "up"], () => {
-        stopCurrentAnimation();
-        viewSummary(table.rows.selected);
-        screen.render();
-      });
-
       screen.key(["right"], () => {
         const selected = table.rows.selected;
-        const bookmark = bookmarks[selected];
+        const bookmark = currentBookmarks[selected];
         const href = bookmark["href"];
         try {
           execSync(`echo ${href} | pbcopy`);
@@ -221,9 +251,96 @@ import { execSync } from "child_process";
         screen.render();
       });
 
+      screen.key(["s"], () => {
+        const searchBox = blessed.textbox({
+          parent: screen,
+          top: "center",
+          left: "center",
+          height: 4,
+          width: "50%",
+          border: "line",
+          style: {
+            border: {
+              fg: "green",
+            },
+          },
+        });
+
+        searchBox.focus();
+
+        searchBox.setContent("");
+        searchBox.readInput();
+
+        searchBox.on("submit", () => {
+          const text = searchBox.getValue();
+
+          alertBox.setContent(`Searching for: ${text}\n`);
+
+          searchBox.setContent("");
+          screen.remove(searchBox);
+
+          if (!text) {
+            return;
+          }
+
+          const results = fuse.search(text);
+          currentBookmarks = results.map((result) => result.item);
+
+          const resultsWithScores = results
+            .map((result) => {
+              return `${result.item.description} (Score: ${result.score})`;
+            })
+            .join("\n");
+
+          alertBox.setContent(
+            `Found ${currentBookmarks.length} results\n\n${resultsWithScores}`
+          );
+          screen.render();
+
+          // update the table with the filtered bookmarks
+          const filteredTableData = {
+            headers: viewHeaders,
+            data: currentBookmarks.map((bookmark) => {
+              return viewHeaders.map((header) => bookmark[header] || "");
+            }),
+          };
+
+          const coloredFilteredTableData = {
+            headers: filteredTableData.headers,
+            data: filteredTableData.data.map((row, index) => {
+              const color = colorScale(currentBookmarks[index].type);
+              return row.map((cell) => chalk.hex(color)(cell));
+            }),
+          };
+
+          table.setData(coloredFilteredTableData);
+          screen.render();
+          searchBox.destroy();
+        });
+
+        screen.render();
+      });
+
+      screen.key(["z"], () => {
+        summaryBox.show();
+        const selected = table.rows.selected;
+        const bookmark = currentBookmarks[selected];
+        const summary = headers
+          .map((header) => `${header}: ${bookmark[header]}`)
+          .join("\n\n");
+
+        summaryBox.setContent(summary);
+        screen.render();
+
+        screen.key(["z"], () => {
+          summaryBox.hide();
+          screen.render();
+        });
+      });
+
       screen.key(["left"], () => {
         const selected = table.rows.selected;
-        const bookmark = bookmarks[selected];
+        const bookmark = currentBookmarks[selected];
         const public_url = bookmark["public_url"];
         try {
           execSync(`echo ${public_url} | pbcopy`);
